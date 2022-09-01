744397c1ceb97ae4e9ea463b3293aa781a0102e3
Pin <pino.chang@tpisoftware.com>
2022-09-01 22:48:03 +0800

    feat: distinct word and resort

---
+++ DistinctWord/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace DistinctWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var distinctContent = Regex.Split(textBox1.Text, "\r\n", RegexOptions.IgnoreCase)
+                .Distinct()
+                .OrderBy(s => s)
+                .ToList();
+            var result = new StringBuilder();
+            foreach (var content in distinctContent)
+            {
+                result.Append(content).Append("\r\n");
+            }
+
+            textBox2.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetDataObject(textBox2.Text);
+        }
+    }
+}
